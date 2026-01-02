8f9609ee6263ece4aabd1cf7571582e8caab1c15
tomasferro-dev <tomasferro.dev>
2026-01-02 14:11:03 -0300

    dependencias modificadas

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [
-    // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
-    react(),
-    tailwindcss(),
-  ],
-  resolve: {
-    alias: {
-      // Alias @ to the src directory
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  }
 })
